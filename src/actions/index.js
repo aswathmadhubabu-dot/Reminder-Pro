43f45bb55882327c2e2543561bb7ada8967ce995
ases573 <ases573@gmail.com>
2017-06-23 16:35:18 +0530

    "[GIT AUTO COMMIT]: Fri Jun 23 2017 16:35:18 GMT+0530 (India Standard Time)"

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,10 @@
-import {ADD_REMINDER} from '../constants.js'
+import {ADD_REMINDER} from '../constants.js';
+
+export const addReminder = function(text){
+    const action ={
+        type: 'ADD_REMINDER',
+        text : text
+    }
+    console.log(action);
+    return action;
+}
