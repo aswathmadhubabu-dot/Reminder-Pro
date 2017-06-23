dd6656e3aa4547018cd9c5418261bda295a1ff9e
ases573 <ases573@gmail.com>
2017-06-23 11:56:41 +0530

    UI setup

---
+++ src/components/App.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import './App.css'
+class App extends Component {
+    render() {
+        return (
+         <div>
+                <div className="card-block">
+                    
+                        <div className="text-center">
+                            <h3><i className="fa fa-tasks"></i> Remember:</h3>
+                            <hr className="mt-2 mb-2" />
+                        </div>
+                        
+                        <div className="md-form">
+                            <i className="fa fa-bookmark"></i>
+                            <input type="text" id="form2" className="form-control" placeholder='i want...' />
+                            
+                        </div>
+                        <div className="md-form">
+                            <i className="fa fa-calendar "></i>
+                            <input type="date" id="form4" className="form-control" placeholder='Date'/>
+                            
+                        </div>
+                        <div className="text-center">
+                            <button className="btn btn-blue-grey">SUBMIT</button>
+                        </div>
+                </div>
+        </div>
+                        );
+    }
+}
+
+export default App;
