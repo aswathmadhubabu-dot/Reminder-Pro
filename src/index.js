a5d688cd85de1417671ff3f378d00131fa00fd9a
ases573 <ases573@gmail.com>
2017-06-24 10:21:33 +0530

    "[GIT AUTO COMMIT]: Sat Jun 24 2017 10:21:33 GMT+0530 (India Standard Time)"

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import ReactDom from 'react-dom';
 import App from './components/App';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import reducer from './reducers';
+import reducer from './reducers'
 
 var store = createStore(reducer);
-
 ReactDom.render( < Provider store = { store } > < App / > < /Provider>, document.getElementById('root'));
