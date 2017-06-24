04ffeebf14560cfbe20020ef093fe2109b775710
ases573 <ases573@gmail.com>
2017-06-24 11:15:33 +0530

    "[GIT AUTO COMMIT]: Sat Jun 24 2017 11:15:33 GMT+0530 (India Standard Time)"

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -27,8 +27,8 @@ class App extends Component {
                     reminders.map(reminder => {
                         return (<li key={reminder.id} className='list-group-item justify-content-between'>
                             <div className='list-group-item '>{reminder.text}    </div>
-                            <button className='btn btn-blue-grey'>Remove &nbsp;&nbsp;<i className="fa fa-times"></i>
-                            onClick ={() =>this.deleteReminder(reminder.id)}
+                            <button className='btn btn-blue-grey'   onClick ={() =>this.deleteReminder(reminder.id)}>Remove &nbsp;&nbsp;<i className="fa fa-times"></i>
+                         
                             
                             </button>
                         
