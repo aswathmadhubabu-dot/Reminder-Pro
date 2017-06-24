140b5dfd86c2c7c9c62d027f42f0b452d7686ca4
ases573 <ases573@gmail.com>
2017-06-24 13:05:33 +0530

    "[GIT AUTO COMMIT]: Sat Jun 24 2017 13:05:33 GMT+0530 (India Standard Time)"

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import { connect } from 'react-redux';
-
+import moment from 'moment';
 import { addReminder } from '../actions';
 import { deleteReminder } from '../actions';
 class App extends Component {
@@ -28,7 +28,7 @@ class App extends Component {
                     reminders.map(reminder => {
                         return (<li key={reminder.id} className='list-group-item justify-content-between'>
                             <div className='list-group-item '>{reminder.text}    </div>
-                            <div className='list-group-item'><em>{reminder.dueDate}</em></div>
+                            <div className='list-group-item'><em>moment(new Date({reminder.dueDate}).fromNow())</em></div>
                             <button className='btn btn-blue-grey' onClick={() => this.deleteReminder(reminder.id)}><i className="fa fa-times"></i>
                             </button>
                              
