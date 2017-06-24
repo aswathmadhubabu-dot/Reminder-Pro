aa424f6f97676a14c60fb3a5740f6ed26672060b
ases573 <ases573@gmail.com>
2017-06-24 13:29:33 +0530

    "[GIT AUTO COMMIT]: Sat Jun 24 2017 13:29:33 GMT+0530 (India Standard Time)"

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -20,13 +20,15 @@ return reminders;
 
 const reminders = function(state = [], action) {
     let reminders = null;
+    state = read_cookie('reminders');
     switch (action.type) {
         case ADD_REMINDER:
             reminders = [...state, reminder(action)];
-            console.log('reminders as state', reminders);
+            bake_cookie('reminders',reminders);
             return reminders;
         case DELETE_REMINDER:
             reminders = removeById(state,action.id);
+            bake_cookie('reminders',reminders);
             return reminders;
         default:
             return state;
