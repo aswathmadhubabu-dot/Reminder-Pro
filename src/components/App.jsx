4d7eaa4872a85120a68c9568df585da430e66d48
ases573 <ases573@gmail.com>
2017-06-24 10:15:33 +0530

    "[GIT AUTO COMMIT]: Sat Jun 24 2017 10:15:33 GMT+0530 (India Standard Time)"

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -21,7 +21,7 @@ class App extends Component {
             <ul className='list-group'>
             {
                 reminders.map(reminder => {
-                    return(<li key ={reminder.id} className='list-group-item'></li>)
+                return(<li key ={reminder.id} className='list-group-item'>{reminder.text}</li>)
                 })
             }
             </ul>
