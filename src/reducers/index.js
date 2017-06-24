d38d15917ee48b61f46dc728390af193ad42cec4
ases573 <ases573@gmail.com>
2017-06-24 07:53:32 +0530

    "[GIT AUTO COMMIT]: Sat Jun 24 2017 07:53:32 GMT+0530 (India Standard Time)"

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,18 @@
 import { ADD_REMINDER } from '../constants';
 
+const reminder = function(action) {
+    return {
+        text: action.text,
+        id: Math.random()
+
+    }
+}
+
 const reminders = function(state = [], action) {
     let reminders = null;
     switch (action.type) {
         case ADD_REMINDER:
-            reminders = [...state, reminders(action)];
+            reminders = [...state, reminder(action)];
             console.log('reminders as state', reminders);
             return reminders;
         default:
