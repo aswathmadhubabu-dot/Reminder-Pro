aa443c4f17a42c7880f66dd3476003eb861fa1d9
ases573 <ases573@gmail.com>
2017-06-24 14:07:33 +0530

    "[GIT AUTO COMMIT]: Sat Jun 24 2017 14:07:33 GMT+0530 (India Standard Time)"

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -59,7 +59,9 @@ class App extends Component {
                         <input type="datetime-local" id="form4" className="form-control" placeholder='Date' onChange={event => this.setState({ 'dueDate': event.target.value })} />
                         <div className='text-center'><button className="btn btn-floating btn-blue-grey" onClick={() => this.addReminder()}><i className='fa fa-plus'></i></button></div>
                         {this.renderReminders()}
+                        <div className='text-center'>
                         <button className='btn btn-warning'   onClick={() => this.props.clearReminders()}><i className='fa fa-gavel'></i></button>
+                        </div>
                     </div>
 
 
